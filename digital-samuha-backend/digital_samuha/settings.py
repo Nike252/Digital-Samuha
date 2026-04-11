189d0f0259ba7de1e615a260c743b1cedd2584d6
Nike252 <nikeshniroula252@gmail.com>
2026-04-11 22:26:53 +0545

    feat: add support for DATABASE_URL environment variable

--- digital-samuha-backend/digital_samuha/settings.py
+++ digital-samuha-backend/digital_samuha/settings.py
@@ -105,16 +105,13 @@ WSGI_APPLICATION = 'digital_samuha.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+import dj_database_url
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST', '127.0.0.1'),
-        'PORT': os.getenv('DB_PORT', '5432'),
-    }
+    'default': dj_database_url.config(
+        default=f"postgres://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST', '127.0.0.1')}:{os.getenv('DB_PORT', '5432')}/{os.getenv('DB_NAME')}",
+        conn_max_age=600
+    )
 }
 
 
