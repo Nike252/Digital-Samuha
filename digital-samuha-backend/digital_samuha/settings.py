8b74e70c53cd4390f09b2e345db6986acde33d96
Nike252 <nikeshniroula252@gmail.com>
2026-04-11 23:19:03 +0545

    fix: allow localhost origins in production for testing

--- digital-samuha-backend/digital_samuha/settings.py
+++ digital-samuha-backend/digital_samuha/settings.py
@@ -194,8 +194,16 @@ if DEBUG:
         f"http://{NETWORK_IP}:5174",
     ]
 else:
-    CORS_ALLOWED_ORIGINS = [o for o in os.getenv('CORS_ALLOWED_ORIGINS', '').split(',') if o]
-    CSRF_TRUSTED_ORIGINS = [o for o in os.getenv('CSRF_TRUSTED_ORIGINS', '').split(',') if o]
+    # Allow local development origins to communicate with production for testing
+    CORS_ALLOWED_ORIGINS = [
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+    ] + [o for o in os.getenv('CORS_ALLOWED_ORIGINS', '').split(',') if o]
+    
+    CSRF_TRUSTED_ORIGINS = [
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+    ] + [o for o in os.getenv('CSRF_TRUSTED_ORIGINS', '').split(',') if o]
 
 CORS_ALLOW_CREDENTIALS = True
 
